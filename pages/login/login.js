cd4e38760f8aa310882a40bb6bee73cc813b7fa4
Neolnevergiveup <1203237924@qq.com>
2019-01-08 19:23:17 +0800

    首页做完了

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,20 +1,28 @@
 // pages/login/login.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-      array: ['腾讯', '百度', '阿里', '网易'],
-      index: ''
-  },
-  /**
-   * 切换公司
-  **/
-  bindPickerChange: function (e) {
-      console.log('picker发送选择改变，携带值为', e.detail.value)
-      this.setData({
-          index: e.detail.value
-      })
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        array: ['腾讯', '百度', '阿里', '网易'],
+        index: ''
+    },
+    /**
+     * 切换公司
+     **/
+    bindPickerChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            index: e.detail.value
+        })
+    },
+    /**
+     * 登录
+     **/
+    login: function () {
+        wx.navigateTo({
+            url: '/pages/index/index'
+        })
+    }
 })
