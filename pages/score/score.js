7483a9ce55a3370a5be5a47149b30c11cad96d1a
Neolnevergiveup <1203237924@qq.com>
2019-01-21 00:48:04 +0800

    成绩列表完成

--- pages/score/score.js
+++ pages/score/score.js
@@ -1,40 +1,46 @@
 // pages/index/index.js
+import Score from '../../api/score/score'
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        list: [
-            {
-                name: '钳工基础',
-                score: 90,
-                date: '12月12日',
-                useTime: '25分20秒'
-            },
-            {
-                name: '钳工基础',
-                score: 90,
-                date: '12月12日',
-                useTime: '25分20秒'
-            },
-            {
-                name: '钳工基础',
-                score: 90,
-                date: '12月12日',
-                useTime: '25分20秒'
-            },
-            {
-                name: '钳工基础',
-                score: 90,
-                date: '12月12日',
-                useTime: '25分20秒'
-            }
-        ]
+        list: []
+    },
+    onLoad: function () {
+        this.getScoreList()
     },
     goBack: function () {
         wx.navigateBack({
             delta: 1
+        })
+    },
+    // 得到成绩列表
+    getScoreList: function () {
+        Score._getScoreList().then(result => {
+            let res = result.data
+            if (res.code == 0) {
+                let list = res.data.list
+                for (let i = 0; i < list.length; i++) {
+                    let time = list[i].use_time
+                    let min = Math.floor(time / 60)
+                    let second = Math.floor(time % 60)
+                    list[i].use_time = min + '分' + second + '秒'
+                    let day = new Date(list[i].created_time * 1000)
+                    let created_time = (day.getMonth() + 1) + '月' + day.getDate() + '日'
+                    list[i].created_time = created_time
+                }
+                this.setData({
+                    list: list
+                })
+            } else {
+                wx.showToast({
+                    title: res.msg,
+                    icon: 'none',
+                    duration: 2000
+                })
+            }
         })
     }
 })
