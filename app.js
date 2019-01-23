b588f78c13d9fcb782fd58256feaa4b6bc052e6b
Neolnevergiveup <1203237924@qq.com>
2019-01-23 09:30:21 +0800

    13212312321312321312

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 //app.js
 App({
   onLaunch: function () {
-      let systemDatas = wx.getSystemInfoSync()
-      let statusBarHeight = systemDatas.statusBarHeight
-      let height = 48
-      if (systemDatas.system.includes('IOS')) {
-          height = 44
-      } else if (systemDatas.system.includes('Android')) {
-          height = 48
-      }
-      this.globalData.headerTop = parseInt(statusBarHeight) + height
-      console.log(this.globalData.headerTop)
+    // 登录
+    // wx.login({
+    //   success: res => {
+    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    //     console.log(res)
+    //   }
+    // })
   },
   globalData: {
-      headerTop: 0
+    userInfo: null
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,9 @@
 // pages/index/index.js
-const app = getApp()
 Page({
     /**
      * 页面的初始数据
      */
-    data: {
-        headerTop: app.globalData.headerTop
-    },
-    onLoad: function () {
-        console.log(app.globalData.headerTop)
-    },
+    data: {},
     goBack: function () {
         wx.removeStorageSync('token')
         wx.reLaunch({
